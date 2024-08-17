4ab53d0560660ec8aac11fad77883f43a02c0f18
dosinabox <39790055+dosinabox@users.noreply.github.com>
2024-08-17 22:52:41 +0200

    add readme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => '564348567',
             'email' => 'admin@elestonia.com',
