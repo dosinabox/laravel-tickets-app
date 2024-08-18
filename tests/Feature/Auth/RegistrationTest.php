2bd65094003e15bcb04ce649d472e76a40c458ae
dosinabox <39790055+dosinabox@users.noreply.github.com>
2024-08-18 22:38:02 +0200

    disable registration test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -9,16 +9,18 @@ class RegistrationTest extends TestCase
 {
     use RefreshDatabase;
 
-    /*public function test_registration_screen_can_be_rendered(): void
+    public function test_registration_screen_can_be_rendered(): void
     {
-        $response = $this->get('/register');
+        $this->markTestSkipped('Registration is disabled.');
+        /*$response = $this->get('/register');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200);*/
     }
 
     public function test_new_users_can_register(): void
     {
-        $response = $this->post('/register', [
+        $this->markTestSkipped('Registration is disabled.');
+        /*$response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
@@ -26,6 +28,6 @@ class RegistrationTest extends TestCase
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
-    }*/
+        $response->assertRedirect(route('dashboard', absolute: false));*/
+    }
 }
