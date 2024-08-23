c402536f7fb8e76e38efef5b6a0817e957b902ce
dosinabox <39790055+dosinabox@users.noreply.github.com>
2024-08-23 14:25:31 +0200

    move api routes to unprotected

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/visitors', [VisitorController::class, 'index'])->name('visitors.index');
-    Route::get('/visitors/{code}', [VisitorController::class, 'show'])->name('visitors.show');
-    Route::get('/search/{query}', [VisitorController::class, 'search'])->name('visitors.search');
 
     Route::get('/ui/visitors/{code}', [VisitorController::class, 'validate'])->name('visitors.ui.validate');
     Route::get('/ui/search', [VisitorController::class, 'list'])->name('visitors.ui.list');
     Route::get('/ui/manage', [VisitorController::class, 'manage'])->name('visitors.ui.manage');
 });
 
+Route::get('/visitors', [VisitorController::class, 'index'])->name('visitors.index');
+Route::get('/visitors/{code}', [VisitorController::class, 'show'])->name('visitors.show');
+Route::get('/search/{query}', [VisitorController::class, 'search'])->name('visitors.search');
 Route::post('/visitors', [VisitorController::class, 'store'])->name('visitors.store');
 Route::post('/visitors/{id}', [VisitorController::class, 'update'])->name('visitors.update');
 Route::delete('/visitors/{id}', [VisitorController::class, 'delete'])->name('visitors.delete');
